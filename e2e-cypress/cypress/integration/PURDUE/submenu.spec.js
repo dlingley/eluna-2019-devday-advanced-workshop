4647050b987fcc82ed0d2e38694eed4d17dee877
dlingley <dlingley@gmail.com>
2019-04-30 17:40:22 -0400

    test change

--- e2e-cypress/cypress/integration/PURDUE/submenu.spec.js
+++ e2e-cypress/cypress/integration/PURDUE/submenu.spec.js
@@ -2,37 +2,8 @@ describe('searchbar-submenu', () => {
   before(() => {
     cy.visit('/search?vid=PURDUE');
   })
-/*
-  describe('submenu', () => {
-    it('exists', function() {
-      cy.get('search-bar-sub-menu > .layout-align-end-center')
-      .should('exist')
-    })
 
-      it('is visible', function() {
-        cy.get('search-bar-sub-menu > .layout-align-end-center')
-        .should('be.visible')
-      })
 
-    describe('has the correct menu items', () => {
-      const submenuItems = [
-        {
-          label: "Provide Feedback",
-          link: "https://nyu.qualtrics.com/jfe/form/SV_blQ3OFOew9vl6Pb?Source=NYU",
-        },
-        {
-          label: "Library Hours",
-          link: "https://guides.nyu.edu/library-hours",
-        }
-      ]
-
-      it(`has ${submenuItems.length} buttons`)
-
-      submenuItems.forEach(({ label, link }, idx) => {
-        it(`has a button with ${label} which opens ${link} when clicked`)
-      })
-    })
-    */
     describe('test search works', () => {
       it('get a search result', function() {
         cy.get('#searchBar')
